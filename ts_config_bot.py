8cb66c22a6707314a35a426bc4e154deea9028bf
AstroRP2020 <82771448+AstroRP2020@users.noreply.github.com>
2021-12-29 03:22:04 +0100

    Update ts_config_bot.py

--- ts_config_bot.py
+++ ts_config_bot.py
@@ -4,18 +4,18 @@ from pytz import timezone
 import discord
 
 # Général
-bot_token = ""  # Token du Bot Discord
+bot_token = "OTI1NTY1NzQzNTA3MDYyNzk2.Ycu-RQ.SzvMHTPHWHwQD9Ul9BEZCl2G0SQ"  # Token du Bot Discord
 embed_color = discord.Color.from_rgb(47, 49, 54)  # Couleur des embeds
 timezone = "Europe/Brussels"  # Timezone des embeds
 
 # Get Reward
 reward_command = "ts!reward"
-reward_channels_ids = []  # Canaux dans lequel on peut exécuter la commande
-reward_necessary_roles_ids = []  # Rôles nécessaires pour exécuter la commande
+reward_channels_ids = [vote-serveur]  # Canaux dans lequel on peut exécuter la commande
+reward_necessary_roles_ids = [Joueur]  # Rôles nécessaires pour exécuter la commande
 reward_amount = '500'  # Total d'argent donné pour la récompense
 
 # Top Serveur
-ts_server_token = ''  # Identifiant API du serveur sur Top Serveur
+ts_server_token = 'ZXCF4B6K7Z'  # Identifiant API du serveur sur Top Serveur
 
 
 # SSH Connection
